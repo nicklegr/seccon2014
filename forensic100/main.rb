51682b674e77fbe68777af8af4a0f22dc99841d5
nicklegr <addr.for+github@gmail.com>
2014-12-07 22:03:43 +0900

    forensic100のWindowsでの作業分を追加

--- forensic100/main.rb
+++ forensic100/main.rb
@@ -1,3 +1,20 @@
-ARGF.each_line do |e|
-  puts e.unpack('u')
+require 'pp'
+
+d = File.read("forensic100_/Export2/381-key.txt")
+
+loop do
+  d = d.encode("UTF-8", "UTF-8", :invalid => :replace, :undef => :replace, :replace => '?')
+  d[/key(\d+)\.txt/]
+  num = $1
+  puts num
+
+  Dir.glob("forensic100_/Export2/*") do |f|
+    # puts f
+    # if f.include?(d)
+    if f.match(/\d+?-key#{num}/)
+    # if f.match(/\d+?-#{num}/)
+      puts f
+      d = File.read(f)
+    end
+  end
 end
